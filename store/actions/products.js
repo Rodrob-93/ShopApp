a175ec20e4211a60109deb6122f71d9cf1a3fb71
Robin Rodrigues <robinrodrigues6499@gmail.com>
2020-01-04 22:52:03 +0530

    Added PATCH and DELETE for products

--- store/actions/products.js
+++ store/actions/products.js
@@ -9,7 +9,6 @@ export const fetchProducts = () => {
   return async dispatch => {
     // any async code you want!
     try {
-      //GET REQUEST
       const response = await fetch(
         'https://shopapp-28785.firebaseio.com/products.json'
       );
@@ -43,13 +42,23 @@ export const fetchProducts = () => {
 };
 
 export const deleteProduct = productId => {
-  return { type: DELETE_PRODUCT, pid: productId };
+  return async dispatch => {
+    const response = await fetch(
+      `https://shopapp-28785.firebaseio.com/products/${productId}.json`,
+      {
+        method: 'DELETE'
+      }
+    );
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+    dispatch({ type: DELETE_PRODUCT, pid: productId });
+  };
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
   return async dispatch => {
     // any async code you want!
-    //POST REQUEST
     const response = await fetch(
       'https://shopapp-28785.firebaseio.com/products.json',
       {
@@ -82,13 +91,34 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return {
-    type: UPDATE_PRODUCT,
-    pid: id,
-    productData: {
-      title,
-      description,
-      imageUrl
+  return async dispatch => {
+    const response = await fetch(
+      `https://shopapp-28785.firebaseio.com/products/${id}.json`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl
+        })
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
     }
+
+    dispatch({
+      type: UPDATE_PRODUCT,
+      pid: id,
+      productData: {
+        title,
+        description,
+        imageUrl
+      }
+    });
   };
 };
